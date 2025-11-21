5b5b3c8942bf7fc28193b5733bd822c1366fc984
paola-figma <paola@marketechdigital.com>
2025-11-20 22:38:21 -0800

    Create oauth-init.js

---
+++ api/oauth-init.js
@@ -0,0 +1,25 @@
+import { google } from "googleapis";
+
+export default function handler(req, res) {
+  try {
+    const clientID = process.env.GOOGLE_CLIENT_ID;
+    const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+    const redirectURI = process.env.GOOGLE_REDIRECT_URI;
+
+    const oauth2Client = new google.auth.OAuth2(
+      clientID,
+      clientSecret,
+      redirectURI
+    );
+
+    const url = oauth2Client.generateAuthUrl({
+      access_type: "offline",
+      prompt: "consent",
+      scope: ["https://www.googleapis.com/auth/gmail.send"]
+    });
+
+    res.status(200).json({ auth_url: url });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
